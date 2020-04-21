16de6b4c28c7857954a34eb6d65fb8aa8068a508
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-21 14:30:54 -0400

    Change display value based off user input

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const noun1 = document.getElementById("noun1");
 const noun2 = document.getElementById("noun2");
 const noun3 = document.getElementById("noun3");
 const noun4 = document.getElementById("noun4");
+const noun5 = document.getElementById("noun5");
 const noun1Input = document.getElementById("noun1Input");
 const noun2Input = document.getElementById("noun2Input");
 const noun3Input = document.getElementById("noun3Input");
 const noun4Input = document.getElementById("noun4Input");
+const noun5Input = document.getElementById("noun5Input");
 
 myButton.addEventListener('click', function (e) {
     e.preventDefault();
@@ -15,4 +17,8 @@ myButton.addEventListener('click', function (e) {
     noun2.innerHTML = noun2Input.value;
     noun3.innerHTML = noun3Input.value;
     noun4.innerHTML = noun4Input.value;
+
+    if (noun5Input.value == "yes") {
+        noun5.style.display = "block";
+    }
 });
