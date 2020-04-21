e0ac128b7944912a53ddd6c4999ca26da46a9abc
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-21 13:43:08 -0400

    Completed DOM Starter Code exercise

---
+++ script.js
@@ -0,0 +1,18 @@
+const myButton = document.getElementById("clickMe");
+const noun1 = document.getElementById("noun1");
+const noun2 = document.getElementById("noun2");
+const noun3 = document.getElementById("noun3");
+const noun4 = document.getElementById("noun4");
+const noun1Input = document.getElementById("noun1Input");
+const noun2Input = document.getElementById("noun2Input");
+const noun3Input = document.getElementById("noun3Input");
+const noun4Input = document.getElementById("noun4Input");
+
+myButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    noun1.innerHTML = noun1Input.value;
+    noun2.innerHTML = noun2Input.value;
+    noun3.innerHTML = noun3Input.value;
+    noun4.innerHTML = noun4Input.value;
+});
